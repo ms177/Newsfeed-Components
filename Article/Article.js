fca6c3d2be28f498a46721eb63aec102cbacc35e
ms177 <59634495+ms177@users.noreply.github.com>
2020-03-19 19:56:45 -0700

    completed article

--- Article/Article.js
+++ Article/Article.js
@@ -88,27 +88,96 @@ const data = [
   }
 ];
 
-/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
+
+
   
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
+// Step 1: Create a function that creates a component. You will want your component to look like the template below: 
+  
+//   <div class="article">
+//     <h2>{title of the article}</h2>
+//     <p class="date">{date of the article}</p>
 
-    {three separate paragraph elements}
+//     {three separate paragraph elements}
 
-    <span class='expandButton'></span>
-  </div>
+//     <span class='expandButton'></span>
+//   </div>
 
-  Hint: You will need to use createElement more than once here!
+//   Hint: You will need to use createElement more than once here!
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+//   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
 
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+const createArticle = (abc) => {
+ 
+  const articleElement = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const firstParagraph = document.createElement('p');
+  const secondParagraph = document.createElement('p');
+  const thirdParagraph = document.createElement('p');
+  const articleButton = document.createElement('span');
+ 
 
-  Step 3: return the entire component.
+   articleElement.append(articleTitle, articleDate, firstParagraph, secondParagraph, thirdParagraph, articleButton);
 
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+  // articleElement.appendChild(articleTitle);
+  // articleElement.appendChild(articleDate);
+  // articleElement.appendChild(firstParagraph);
+  // articleElement.appendChild(secondParagraph);
+  // articleElement.appendChild(thirdParagraph);
+  // articleElement.appendChild(articleButton);
 
-  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
+  articleElement.classList.add('article');
+  articleDate.classList.add('date');
+  articleButton.classList.add('expandButton');
 
-*/
+  articleTitle.textContent = abc.title;
+  articleDate.textContent = abc.date;
+  firstParagraph.textContent = abc.firstParagraph;
+  secondParagraph.textContent = abc.secondParagraph;
+  thirdParagraph.textContent = abc.thirdParagraph;
+  articleButton.textContent = '\u25bc';
+
+// Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+
+  articleButton.addEventListener('click', () => {
+    articleElement.classList.toggle('article-open');
+  });
+
+  //   Step 3: return the entire component.
+
+  return articleElement;
+
+}
+
+//   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+const articleArray = document.querySelector('.articles');
+
+
+data.forEach(item => articleArray.appendChild(createArticle(item)));
+
+
+
+//   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
+
+articleArray.appendChild(createArticle({
+  title: 'New Title"',
+  date: 'Nov 5th, 2018',
+  firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+      moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
+      watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
+      Hutt calamari darth jabba.Darth dooku amidala organa moff.Boba darth binks solo hutt skywalker dantooine skywalker.Qui - gonn
+      jar twi'lek jinn leia jango skywalker mon.`,
+
+  secondParagraph: `Grievous fett calamari anakin skywalker hutt.Alderaan darth kenobi darth r2- d2
+      windu mothma.Sidious darth calamari moff.Wampa mothma sith wedge solo mara.Darth gonk maul sith moff chewbacca palpatine
+      mace amidala.C - 3po solo skywalker anakin yoda leia.Maul wampa bespin watto jade ewok darth jabba.Lando dantooine moff
+      k - 3po dantooine luke.Fisto mandalore darth wedge c - 3p0 ahsoka.Secura moff palpatine fett.Anakin sith darth darth.Moff
+      solo leia ben ponda jade.Binks jango aayla skywalker skywalker cade.Mustafar darth ventress anakin watto.Yavin jawa sebulba
+      owen jinn tatooine sith organa.`,
+
+  thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+      naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
+      han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
+      moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
+}));
+
